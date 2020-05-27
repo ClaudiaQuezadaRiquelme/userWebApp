ddc10f942b8f6c5e8f487069d47dd86c34a63f6f
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2020-05-27 19:13:57 -0400

    initialize components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './components/info/info.component';
+import { CardComponent } from './components/card/card.component';
+import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/login/login.component';
+import { InitComponent } from './components/init/init.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardContainerComponent } from './components/card-container/card-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    CardComponent,
+    UserComponent,
+    LoginComponent,
+    InitComponent,
+    HeaderComponent,
+    CardContainerComponent
   ],
   imports: [
     BrowserModule,
