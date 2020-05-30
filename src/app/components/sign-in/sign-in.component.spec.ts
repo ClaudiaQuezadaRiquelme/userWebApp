68521f3758b3a83cd07fcdc7dac24ae5066b02bf
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2020-05-30 01:37:33 -0400

    sign-in test works except 2 tests

--- src/app/components/sign-in/sign-in.component.spec.ts
+++ src/app/components/sign-in/sign-in.component.spec.ts
@@ -2,6 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignInComponent } from './sign-in.component';
 import { AuthService } from '../../services/auth.service';
 
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
@@ -10,6 +18,12 @@ describe('SignInComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        ReactiveFormsModule
+      ],
       declarations: [ SignInComponent ],
       providers: [
           { provide: AuthService, useValue: spyAuthService }
@@ -31,14 +45,73 @@ describe('SignInComponent', () => {
 
   // Our testing
   it('should render form with email and password inputs', () => {
-    const element = fixture.nativeElement;
+    const element = fixture.debugElement.nativeElement;
     expect(element.querySelector('form')).toBeTruthy();
-    expect(element.querySelector('#email')).toBeTruthy();
-    expect(element.querySelector('#password')).toBeTruthy();
+    expect(element.querySelector('input[id="userName"]')).toBeTruthy();
+    expect(element.querySelector('input[id="userPassword"]')).toBeTruthy();
     expect(element.querySelector('button')).toBeTruthy();
   });
 
-  // it('should return model invalid when form is empty', () => {
-  //   expect(component.form.valid).toBeFalsy();
-  // });
+  it('should return model invalid when form is empty', () => {
+    expect(component.form.valid).toBeFalsy();
+  });
+
+  it('should validate email input as required', () => {
+    const email = component.form.controls.email;
+    expect(email.valid).toBeFalsy();
+    expect(email.errors.required).toBeTruthy();
+  });
+
+  it('should validate password input as required', () => {
+    const password = component.form.controls.password;
+    expect(password.valid).toBeFalsy();
+    expect(password.errors.required).toBeTruthy();
+  });
+
+  it('should validate email format', () => {
+    const email = component.form.controls.email;
+    email.setValue('test');
+    const errors = email.errors;
+
+    expect(errors.required).toBeFalsy();
+    expect(errors.pattern).toBeTruthy();
+    expect(email.valid).toBeFalsy();
+  });
+
+  it('should validate email format correctly', () => {
+    const email = component.form.controls.email;
+    email.setValue('test@test.com');
+    const errors = email.errors || {};
+
+    expect(email.valid).toBeTruthy();
+    expect(errors.required).toBeFalsy();
+    expect(errors.pattern).toBeFalsy();
+  });
+
+  it('should render email validation message when formControl is submitted and invalid', () => {
+    const elements: HTMLElement = fixture.nativeElement;
+    expect(elements.querySelector('#email-error')).toBeFalsy();
+
+    elements.querySelector('button').click();
+
+    fixture.detectChanges();
+    expect(elements.querySelector('#email-error')).toBeTruthy();
+    expect(elements.querySelector('#email-error').textContent).toContain(
+      'Please enter a valid email.'
+    );
+  });
+
+  it('should render password validation message when formControl is submitted and invalid', () => {
+    const elements: HTMLElement = fixture.nativeElement;
+    expect(elements.querySelector('#password-error')).toBeFalsy();
+
+    elements.querySelector('button').click();
+ 
+    fixture.detectChanges();
+    expect(elements.querySelector('#password-error')).toBeTruthy();
+    expect(elements.querySelector('#password-error').textContent).toContain(
+      'Please enter a valid password.'
+    );
+  });
+
 });
--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,12 +8,23 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  form: FormGroup;
+  submitted = false;
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern('[^ @]*@[^ @]*')]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
   }
 
 }
